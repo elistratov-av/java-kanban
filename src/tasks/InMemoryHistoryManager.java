74bd2a8451977106cd3b3ecef160ca67f3a24685
Елистратов <elistratov-av@yandex.ru>
2024-05-08 12:38:17 +0300

    6 спринт: реализовать новый функционал истории

--- src/tasks/InMemoryHistoryManager.java
+++ src/tasks/InMemoryHistoryManager.java
@@ -2,22 +2,79 @@ package tasks;
 
 import services.HistoryManager;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class InMemoryHistoryManager implements HistoryManager {
-    private static final int MAX_HISTORY_SIZE = 10;
-    private final LinkedList<Task> list = new LinkedList<>();
+
+    private static class Node<T> {
+        public T data;
+        public Node<T> next;
+        public Node<T> prev;
+
+        public Node(Node<T> prev, T data, Node<T> next) {
+            this.data = data;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+
+    private final HashMap<Integer, Node<Task>> map = new HashMap<>();
+    private Node<Task> head;
+    private Node<Task> tail;
+    private int size = 0;
+
+    private void linkLast(Task task) {
+        final Node<Task> oldTail = tail;
+        final Node<Task> newNode = new Node<>(oldTail, task, null);
+        tail = newNode;
+        if (oldTail == null)
+            head = newNode;
+        else
+            oldTail.next = newNode;
+        ++size;
+    }
+
+    private void removeNode(Node<Task> node) {
+        if (node.prev != null)
+            node.prev.next = node.next;
+        else
+            head = node.next;
+        if (node.next != null)
+            node.next.prev = node.prev;
+        else
+            tail = node.prev;
+        --size;
+    }
+
+    private List<Task> getTasks() {
+        List<Task> results = new ArrayList<>(size);
+        for (Node<Task> node = head; node != null; node = node.next) {
+            results.add(node.data);
+        }
+        return results;
+    }
 
     @Override
     public void add(Task task) {
-        list.add(task);
-        if (list.size() > MAX_HISTORY_SIZE)
-            list.removeFirst();
+        Node<Task> node = map.get(task.getId());
+        if (node != null)
+            removeNode(node);
+        linkLast(task);
+        map.put(task.getId(), tail);
+    }
+
+    @Override
+    public void remove(int id) {
+        Node<Task> node = map.remove(id);
+        if (node != null)
+            removeNode(node);
     }
 
     @Override
     public List<Task> getHistory() {
-        return list;
+        return getTasks();
     }
 }
+
